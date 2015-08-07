bcc93babf7ba0214bf3d77b03e4ffbd56ef263b4
superanguri <periklis.t@outlook.com>
2015-08-07 21:39:08 +0200

    Update Main.java

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,9 +1,11 @@
 package calculator;
 
 public class Main {
+  
+  private Calculator frame = new Calculator();
   public static void main(String[] args) {
-    Calculator frame = new Calculator();
     
+    frame.setVisible(true);
     
     
   }
