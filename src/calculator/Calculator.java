131626a468b8fcafcfe752b8cd228c310a8740a0
superanguri <periklis.t@outlook.com>
2015-08-07 21:46:12 +0200

    Update Calculator.java

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -8,6 +9,7 @@ import javax.swing.JTextField;
 public class Calculator extends JFrame{
 
     private static Calculator instance = null;
+    private Dimension fSize = new Dimension(235, 329);
     
     private JPanel result = new JPanel(null);
     private JTextField input = new JTextField();
@@ -17,8 +19,10 @@ public class Calculator extends JFrame{
         
         super.setLayout(new BorderLayout(20, 20));
         super.setTitle("Calculator");
+        super.setSize(fSize);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setLocationRelativeTo(null);
+        super.setVisible(true);
         
     }
     
