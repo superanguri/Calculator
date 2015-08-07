74d45a99f41786fb8913b798db3c42ee5bdc81f7
superanguri <periklis.t@outlook.com>
2015-08-07 23:08:37 +0200

    Update Function.java

--- src/calculator/Function.java
+++ src/calculator/Function.java
@@ -4,7 +4,7 @@ public class Function{
 
     public static double addiere(double[] zahlen){
         
-        double ergebnis = null;
+        double ergebnis = 0;
         
         
         return ergebnis;
@@ -12,7 +12,7 @@ public class Function{
     
     public static double subtrahiere(double[] zahlen){
         
-        double ergebnis = null;
+        double ergebnis = 0;
         
         
         return ergebnis;
@@ -20,18 +20,25 @@ public class Function{
     
     public static double multipliziere(double[] zahlen){
         
-        double ergebnis = null;
+        double ergebnis = 0;
         
         
         return ergebnis;    
         
     }
     
-    private static <T> T dividiere(T[] zahlen){
+    public static double dividiere(double[] zahlen){
         
-        T ergebnis = null;
+        double ergebnis = 0;
         
         
         return ergebnis;
+    }
+    
+    public static boolean isNumber(char c){
+        if((c-48) < 10)
+            return true;
+        
+        return false;
     }
 }
