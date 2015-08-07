9cdfa5472fbe1c5f58a0039ccff8333a6ebaee40
superanguri <periklis.t@outlook.com>
2015-08-07 21:36:17 +0200

    Update Calculator.java

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,13 +1,25 @@
 package calculator;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Calculator extends JFrame{
 
     private static Calculator instance = null;
     
+    private JPanel result = new JPanel(null);
+    private JTextField input = new JTextField();
+    
     private Calculator(){
         
+        
+        super.setLayout(new BorderLayout(20, 20));
+        super.setTitle("Calculator");
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setLocationRelativeTo(null);
+        super.setVisible(true);
         
     }
     
